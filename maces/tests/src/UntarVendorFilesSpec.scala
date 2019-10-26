acc6345c72a0a707e6040dc59dacdc1c52d3cab3
Jiuyang liu <liujiuyang1994@gmail.com>
2019-10-26 11:31:46 +0000

    more elegant tests

--- maces/tests/src/UntarVendorFilesSpec.scala
+++ maces/tests/src/UntarVendorFilesSpec.scala
@@ -10,8 +10,7 @@ import utest.framework.Formatter
 
 abstract class MacesTestSuite extends TestSuite {
   def sanitizePath(path: FilePath): FilePath = Path(path.toString.replaceAll(" ", "_").replaceAll("\\W+", ""))
-
-
+  
   def testPath()(implicit testPath: utest.framework.TestPath) = Path("test_run_dir", pwd)
 
   def resourcesDir = Path("maces/tests/resources", pwd)
@@ -27,12 +26,19 @@ abstract class MacesTestSuite extends TestSuite {
 object UntarVendorFilesSpec extends MacesTestSuite {
   val tests = Tests {
     test("annotated tar should be extracted") {
+      rm ! testPath
       val scratchPad = ScratchPad(Set(
         Annotation("vendor.tars", TarsPathAnnotationValue(Seq(resourcesDir / "files.tar.xz"))),
         Annotation("vendor.internal_tars", RelTarsPathAnnotationValue(Seq(RelPath("files.tar.xz/internal_files.tar")))),
         Annotation("system.tar_cache", DirectoryPathAnnotationValue(testPath / "tar_cache"))
       ))
       UntarStage(scratchPad).scratchPadOut
+      assert((testPath / "tar_cache" / "files.tar.xz").isDir)
+      assert((testPath / "tar_cache" / "files.tar.xz" / "someFile0").isFile)
+      assert((testPath / "tar_cache" / "files.tar.xz" / "someFile0").isFile)
+      assert((testPath / "tar_cache" / "files.tar.xz" / "internal_files.tar").isDir)
+      assert((testPath / "tar_cache" / "files.tar.xz" / "internal_files.tar" / "someInternalFile0").isFile)
+      assert((testPath / "tar_cache" / "files.tar.xz" / "internal_files.tar" / "someInternalFile1").isFile)
     }
   }
 }
